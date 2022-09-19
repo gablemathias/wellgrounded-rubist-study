b8cc1a41ac4846c38d936f033a938be92b87b64c
Gabriel Mathias <gablemathias@icloud.com>
2022-09-19 18:55:35 -0300

    Add f to c converter

---
+++ celsius_to_fahrenheit/f2cio.rb
@@ -0,0 +1,7 @@
+puts "Reading Fahrenheit temperature value from data file..."
+fahrenheit = File.read('temp.dat') # (celsius * 9 / 5) + 32
+celsius = (fahrenheit.to_i - 32) * 5 / 9 # (32°F − 32) × 5/9
+puts "Saving result to output file 'temp.out'"
+cs = File.new('temp.out', 'w')
+cs.puts celsius
+cs.close
