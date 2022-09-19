ca60f0173dd73b33746ae1e400bae3d9c0692406
Gabriel Mathias <gablemathias@icloud.com>
2022-09-19 18:21:52 -0300

    add gets celsius from userinput

--- celsius_to_fahrenheit.rb
+++ celsius_to_fahrenheit.rb
@@ -1,5 +1,6 @@
-celsius = 100
-fahrenheit = (celsius * 9 / 5) + 32
+print "Hello. Please enter a Celsius value: "
+celsius = gets
+fahrenheit = (celsius.to_i * 9 / 5) + 32
 print "The result is "
 print fahrenheit
 puts "."
