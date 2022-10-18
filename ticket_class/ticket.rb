2950eb88bf57102b48b0a0d48392e3cffffc7b3f
Gabriel Mathias <gablemathias@icloud.com>
2022-10-17 22:55:13 -0300

    test methods with same name

--- ticket_class/ticket.rb
+++ ticket_class/ticket.rb
@@ -2,4 +2,17 @@ class Ticket
   def event
     "Can't really be specified yet..."
   end
+
+  def price
+    "The first price is $5"
+  end
+
+  def price
+    "Now the price is $7"
+  end
 end
+
+ticket = Ticket.new
+
+puts ticket.event
+puts ticket.price
